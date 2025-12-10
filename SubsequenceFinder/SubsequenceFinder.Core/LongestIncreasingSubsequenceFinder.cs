2f6cb8ed2aa285b3af56d5b483770388483c3d65
Rendy <ryoma555@gmail.com>
2025-12-10 16:07:15 +1100

    Implements the logic to find the longest consecutive numbers

--- SubsequenceFinder/SubsequenceFinder.Core/LongestIncreasingSubsequenceFinder.cs
+++ SubsequenceFinder/SubsequenceFinder.Core/LongestIncreasingSubsequenceFinder.cs
@@ -2,9 +2,38 @@
 {
     public class LongestIncreasingSubsequenceFinder
     {
-        public string FindLongestIncreasingSubsequence()
+        public string FindLongestIncreasingSubsequence(string input)
         {
-            throw new NotImplementedException();
+            List<int> longestNumbers = [];
+            int currentMaxLength = 0;
+            List<int> increasingNumbers = [];
+            
+            var numbers = input.Split(' ').Select(int.Parse).ToArray();
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(increasingNumbers.Count == 0 || numbers[i] > increasingNumbers.Last())
+                {
+                    increasingNumbers.Add(numbers[i]);
+                }
+                else
+                {
+                    if(increasingNumbers.Count > currentMaxLength)
+                    {
+                        currentMaxLength = increasingNumbers.Count;
+                        longestNumbers = [.. increasingNumbers];
+                    }
+                    increasingNumbers = [numbers[i]];
+                }
+            }
+
+            if (increasingNumbers.Count > currentMaxLength)
+            {
+                currentMaxLength = increasingNumbers.Count;
+                longestNumbers = increasingNumbers;
+            }
+
+            return string.Join(" ", longestNumbers);
         }
     }
 }
