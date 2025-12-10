f46a29eec778df9749ae72ceb9f77d3daced7138
Rendy <ryoma555@gmail.com>
2025-12-10 16:08:02 +1100

    Adds long input/output test data to folder

--- SubsequenceFinder/SubsequenceFinder.Tests/LongestIncreasingSubsequenceFinderTests.cs
+++ SubsequenceFinder/SubsequenceFinder.Tests/LongestIncreasingSubsequenceFinderTests.cs
@@ -1,7 +1,36 @@
-﻿namespace SubsequenceFinder.Tests
+﻿using SubsequenceFinder.Core;
+
+namespace SubsequenceFinder.Tests
 {
     public class LongestIncreasingSubsequenceFinderTests
     {
+        private const string TestDataFolder = "TestData";
+
+        public static IEnumerable<object[]> AllTestData =>
+            Directory.EnumerateDirectories(Path.Combine(Directory.GetCurrentDirectory(), TestDataFolder))
+                .Select(testCaseDir => new
+                {
+                    InputPath = Path.Combine(testCaseDir, "Input.txt"),
+                    OutputPath = Path.Combine(testCaseDir, "Output.txt"),
+                })
+                .Where(fileData => File.Exists(fileData.InputPath) && File.Exists(fileData.OutputPath))
+                .Select(fileData => new object[]
+                {
+                File.ReadAllText(fileData.InputPath).Trim(),
+                File.ReadAllText(fileData.OutputPath).Trim(),
+                });
+
+        [Theory]
+        [MemberData(nameof(AllTestData))]
+        public void FindLongestIncreasingSubsequence_ShouldHandleLargeInputs(string input, string expected)
+        {
+            var finder = new LongestIncreasingSubsequenceFinder();
+            var output = finder.FindLongestIncreasingSubsequence(input);
+
+            Assert.Equal(expected, output);
+        }
+
+
         [Theory]
         [InlineData("6 1 5 9 2", "1 5 9")]
         [InlineData("6 2 4 6 1 5 9 2", "2 4 6")]
