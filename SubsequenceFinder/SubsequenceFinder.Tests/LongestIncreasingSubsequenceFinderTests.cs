2fb3c2728a0af25af1f4bb978d4bdbf0affa2c77
Rendy <ryoma555@gmail.com>
2025-12-10 12:24:01 +1100

    Adds unit tests with small input

--- SubsequenceFinder/SubsequenceFinder.Tests/LongestIncreasingSubsequenceFinderTests.cs
+++ SubsequenceFinder/SubsequenceFinder.Tests/LongestIncreasingSubsequenceFinderTests.cs
@@ -2,10 +2,16 @@
 {
     public class LongestIncreasingSubsequenceFinderTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("6 1 5 9 2", "1 5 9")]
+        [InlineData("6 2 4 6 1 5 9 2", "2 4 6")]
+        [InlineData("6 2 4 3 1 5 9", "1 5 9")]
+        public void FindLongestIncreasingSubsequence_ShouldHandleSmallInputs(string input, string expected)
         {
+            var finder = new Core.LongestIncreasingSubsequenceFinder();
+            var output = finder.FindLongestIncreasingSubsequence(input);
 
+            Assert.Equal(expected, output);
         }
     }
 }
